54c162bc74d95b344afbffc24169beea58077a7e
Miquel <miquelalessandro@gmail.com>
2019-11-25 14:51:30 +0100

    models aangemaakt

---
+++ SA/Models/party.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SA.Models
+{
+    public class Party
+    {
+        public int ID { set; get; }
+        public string name { set; get; }
+        public string leader { set; get; }
+        public int parlement_chairs { set; get; }
+        public int senate_chairs { set; get; }
+
+
+    }
+}
---
+++ SA/Models/party_abbreviations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SA.Models
+{
+    public class party_abbreviations
+    {
+        public int ID { set; get; }
+        public string abbreviation { set; get; }
+    }
+}
