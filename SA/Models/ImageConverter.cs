ff3d9ca2001f917c0eb951e2944ac1be75b0e265
Andy <496993@edu.rocmn.nl>
2019-11-26 12:18:35 +0100

    Fixed the dashboard

--- SA/Models/ImageConverter.cs
+++ SA/Models/ImageConverter.cs
@@ -6,7 +6,7 @@ using Xamarin.Forms;
 
 namespace SA.Models
 {
-    public class ImageConverter : IValueConverter
+     public class ImageConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
