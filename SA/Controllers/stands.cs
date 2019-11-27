6454800054956b005a7eb3199e179be5bb2d4875
YoussefAlmardini <yousef.mardini@gmail.com>
2019-11-27 09:06:05 +0100

    pull request

---
+++ SA/Controllers/stands.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SA.Data;
+using SA.Models;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SA.Controllers
+{
+    internal class StandController
+    {
+        public static List<Stand> GetStands()
+        {
+            var request = DatabaseService.Request("http://520351.student4a7.ao-ica.nl/api/getStands.php");
+            using (var s = request.GetResponse().GetResponseStream())
+            {
+                using (var sr = new StreamReader(s))
+                {
+                    var result = sr.ReadToEnd();
+                    var data = JsonConvert.DeserializeObject<List<Stand>>(result);
+                    return data;
+                }
+            }
+        }
+    }
+}
