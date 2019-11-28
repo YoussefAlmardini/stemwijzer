bda071d898872a57ae6bf205eb95bdf07cbb0f6e
YoussefAlmardini <yousef.mardini@gmail.com>
2019-11-28 13:11:14 +0100

    result backend added

--- SA/Models/party.cs
+++ SA/Models/party.cs
@@ -13,5 +13,48 @@ namespace SA.Models
         public int parlement_chairs { set; get; }
         public int senate_chairs { set; get; }
 
+        public decimal points
+        {
+            set;
+            get;
+        }
+        public decimal adviced_percentage { set; get; }
+    }
+
+    public class PartyAdvice
+    {
+        public static List<Party> GetParties(List<Standpoints> partiesStandPoints)
+        {
+            List<Party> parties = new List<Party>();
+
+            for (int i = 0; i < partiesStandPoints.Count; i++)
+            {
+                bool doubleRecord = false;
+
+                for (int j = 0; j < parties.Count; j++)
+                {
+                    if (parties[j].name == partiesStandPoints[i].name)
+                    {
+                        doubleRecord = true;
+                        break;
+                    }
+                }
+
+                if (!doubleRecord)
+                {
+                    Party currentParty = new Party();
+                    currentParty.name = partiesStandPoints[i].name;
+                    currentParty.leader = partiesStandPoints[i].leader;
+                    currentParty.parlement_chairs = Convert.ToInt32(partiesStandPoints[i].parlement_chairs);
+                    currentParty.senate_chairs = Convert.ToInt32(partiesStandPoints[i].senate_chairs);
+                    currentParty.abbreviation = partiesStandPoints[i].abbreviation;
+                    currentParty.points = 0;
+                    parties.Add(currentParty);
+                }
+
+            }//End for loop
+
+            return parties;
+        }
     }
 }
--- SA/Views/ResultsView.xaml.cs
+++ SA/Views/ResultsView.xaml.cs
@@ -14,14 +14,64 @@ namespace SA.Views
     public partial class ResultsView : ContentPage
     {
         User __user;
+        List<Standpoints> __partiesStandPoints;
+        List<Advice> adviceList;
         public ResultsView()
         {
             InitializeComponent();
         }
-        public ResultsView(User user)
+        public ResultsView(User user, List<Standpoints> partiesStandPoints)
         {
             InitializeComponent();
+            InitializeData(user, partiesStandPoints);
+        }
+
+
+        private void InitializeData(User user, List<Standpoints> partiesStandPoints)
+        {
             this.__user = user;
+            this.__partiesStandPoints = partiesStandPoints;
+            List<Party> parties = this.GetAdvice();
+            var test = parties;
+        }
+
+        private List<Party> GetAdvice()
+        {
+            adviceList = new List<Advice>();
+
+            var userOpinion = __user.Session.UserOpinion;
+            var partiesOpinion = __partiesStandPoints;
+            var parties = PartyAdvice.GetParties(__partiesStandPoints);
+
+            for (int i = 0; i < userOpinion.Count; i++)
+            {
+                var currentUserOpinion = userOpinion[i];
+                Advice advice = new Advice();
+
+                for (int j = 0; j < partiesOpinion.Count; j++)
+                {
+                    //If current stand is equal to party stand
+                    if (currentUserOpinion.stand.ID == partiesOpinion[j].stand_id)
+                    {
+                        Standpoints currentStandPoint = partiesOpinion[j];
+
+                        for (int x = 0; x < parties.Count; x++)
+                        {
+                            if (parties[x].name == currentStandPoint.name)
+                            {
+                                parties[x].points += advice.GetPoint(currentUserOpinion.userOpinion, currentStandPoint);
+                            }
+                        }
+                    }
+                }
+
+            }
+            for (int i = 0; i < parties.Count; i++)
+            {
+                parties[i].adviced_percentage = (parties[i].points * 100) / 10;
+            }
+
+           return parties;
         }
     }
 }
